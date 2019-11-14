f1af6df9da6f9341eec378c50f41b61dbd57429e
Gavin <gafr4910@colorado.edu>
2019-11-13 20:34:25 -0700

    Gavin scene, w/ Jawsh Sprite move, walk+idle animations, interactables

---
+++ C@ck Block Game/Assets/Scripts/Item.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string itemName;
+    public bool isInRange = false;
+    public bool isPickedUp = false;
+    
+    void Start()
+    {
+        itemName = name;
+    }
+
+    void Update()
+    {
+        if(isInRange && Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("yep");
+            isPickedUp = true;
+            this.gameObject.SetActive(false);
+        }
+        //Debug.Log(isPickedUp);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject otherGO = other.gameObject;
+        //Debug.Log(other);
+        if(otherGO.name == "Jawsh")
+        {
+            isInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        GameObject otherGO = other.gameObject;
+        //Debug.Log(other);
+        if(otherGO.name == "Jawsh")
+        {
+            isInRange = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        GameObject otherGO = other.gameObject;
+        //Debug.Log(other);
+        if(otherGO.name == "Jawsh")
+        {
+            isInRange = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        GameObject otherGO = other.gameObject;
+        if(otherGO.name == "Jawsh")
+        {
+            isInRange = false;
+        }
+    }
+
+}
---
+++ C@ck Block Game/Assets/Scripts/JawshMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JawshMovement : MonoBehaviour
+{
+    public float speed = 10;
+    public KeyCode left, right, up, down;
+
+    private Rigidbody2D     rb;
+	private Animator anim;
+	private SpriteRenderer sr;
+
+    void Awake () 
+    {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		sr = GetComponent<SpriteRenderer>();
+	}
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // float xAxis = Input.GetAxis("Horizontal");
+        // float yAxis = Input.GetAxis("Vertical");
+        // Vector3 pos = transform.position;
+        // pos.x += xAxis * speed * Time.deltaTime;
+        // pos.y += yAxis * speed * Time.deltaTime;
+        // transform.position = pos;
+
+        if(Input.GetKey(left))
+        {
+			rb.velocity = Vector2.left * speed;
+			anim.CrossFade("Jawsh_Walk", 0);
+			sr.flipX = true;
+		}
+
+		else if(Input.GetKey(right))
+        {
+			rb.velocity = Vector2.right * speed;
+			anim.CrossFade("Jawsh_Walk", 0);
+			sr.flipX = false;
+		}
+
+        else if(Input.GetKey(up))
+        {
+			rb.velocity = Vector2.up * speed;
+			anim.CrossFade("Jawsh_Walk", 0);
+		}
+
+        else if(Input.GetKey(down))
+        {
+			rb.velocity = Vector2.down * speed;
+			anim.CrossFade("Jawsh_Walk", 0);
+		}
+
+        else 
+		{
+            rb.velocity = Vector2.up * 0;
+			anim.CrossFade("Jawsh_Idle", 0);
+		}
+    }
+}
