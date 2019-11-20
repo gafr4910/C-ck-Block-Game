f5c0ceaf82c674ca3d9b49d1ff37073a412539fb
Gavin <gafr4910@colorado.edu>
2019-11-20 13:01:21 -0700

    Diagonal movement and shift to run w/ animation

--- C@ck Block Game/Assets/Scripts/JawshMovement.cs
+++ C@ck Block Game/Assets/Scripts/JawshMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class JawshMovement : MonoBehaviour
 {
     public float speed = 10;
+    public float runSpeed = 15;
     public KeyCode left, right, up, down;
 
     private Rigidbody2D     rb;
@@ -25,43 +26,72 @@ public class JawshMovement : MonoBehaviour
 
     void Update()
     {
-        // float xAxis = Input.GetAxis("Horizontal");
-        // float yAxis = Input.GetAxis("Vertical");
-        // Vector3 pos = transform.position;
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        //Vector3 pos = transform.position;
+        if(Input.GetAxis("Fire3") == 0)
+        {
+            rb.velocity = Vector2.right * xAxis * speed + Vector2.up * yAxis * speed;
+        }
+        else 
+        {
+            rb.velocity = Vector2.right * xAxis * runSpeed + Vector2.up * yAxis * runSpeed;
+        }
         // pos.x += xAxis * speed * Time.deltaTime;
         // pos.y += yAxis * speed * Time.deltaTime;
-        // transform.position = pos;
-
-        if(Input.GetKey(left))
+        //transform.position = pos;
+        if(xAxis == 0 && yAxis == 0)
         {
-			rb.velocity = Vector2.left * speed;
-			anim.CrossFade("Jawsh_Walk", 0);
-			sr.flipX = true;
-		}
-
-		else if(Input.GetKey(right))
+            anim.CrossFade("Jawsh_Idle", 0);
+        }
+        else if(Input.GetAxis("Fire3") > 0)
         {
-			rb.velocity = Vector2.right * speed;
-			anim.CrossFade("Jawsh_Walk", 0);
-			sr.flipX = false;
-		}
-
-        else if(Input.GetKey(up))
+            anim.CrossFade("Jawsh_Run", 0);
+            sr.flipX = xAxis < 0;
+        }
+        else
         {
-			rb.velocity = Vector2.up * speed;
-			anim.CrossFade("Jawsh_Walk", 0);
-		}
+            anim.CrossFade("Jawsh_Walk", 0);
+            sr.flipX = xAxis < 0;
+        }
 
-        else if(Input.GetKey(down))
-        {
-			rb.velocity = Vector2.down * speed;
-			anim.CrossFade("Jawsh_Walk", 0);
-		}
+        // float horiz = 0.0f;
+        // float vert = 0.0f;
 
-        else 
-		{
-            rb.velocity = Vector2.up * 0;
-			anim.CrossFade("Jawsh_Idle", 0);
-		}
+        // Debug.Log(Input.GetKey(left) + ", " + Input.GetKey(right) + ", " + Input.GetKey(up) + ", " + Input.GetKey(down));
+
+        // if(Input.GetKey(left))
+        // {
+        //     // horiz -= Input.GetKey(left);
+		// 	rb.velocity = Vector2.left * speed;
+		// 	anim.CrossFade("Jawsh_Walk", 0);
+		// 	sr.flipX = true;
+		// }
+
+		// else if(Input.GetKey(right))
+        // {
+        //     // horiz += Input.GetKey(right);
+		// 	rb.velocity = Vector2.right * speed;
+		// 	anim.CrossFade("Jawsh_Walk", 0);
+		// 	sr.flipX = false;
+		// }
+
+        // else if(Input.GetKey(up))
+        // {
+		// 	rb.velocity = Vector2.up * speed;
+		// 	anim.CrossFade("Jawsh_Walk", 0);
+		// }
+
+        // else if(Input.GetKey(down))
+        // {
+		// 	rb.velocity = Vector2.down * speed;
+		// 	anim.CrossFade("Jawsh_Walk", 0);
+		// }
+
+        // else 
+		// {
+        //     rb.velocity = Vector2.up * 0;
+		// 	anim.CrossFade("Jawsh_Idle", 0);
+		// }
     }
 }
