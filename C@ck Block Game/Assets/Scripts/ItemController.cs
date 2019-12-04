556d8e037a78c5e0230d8924d7744093dfed5164
Gavin <gafr4910@colorado.edu>
2019-12-04 12:58:44 -0700

    Disguise functionality added

--- C@ck Block Game/Assets/Scripts/ItemController.cs
+++ C@ck Block Game/Assets/Scripts/ItemController.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class ItemController : MonoBehaviour
 {
     public GameObject[] Items;
-
-    void Start()
-    {
-        
-    }
+    public GameObject[] FakeProposalScenario;
+    public GameObject Jawsh;
 
     void Update()
     {
-        
+        foreach(GameObject i in Items)
+        {
+            if(i.name == "Waiter Bundle" && !i.activeSelf)
+            {
+                GameObject j = GameObject.Find("Jawsh");
+                JawshMovement jMove = j.GetComponent<JawshMovement>();
+                jMove.hasWaiterDisguise = true;
+            }
+        }
     }
 }
